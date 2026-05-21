68d6868179396434ffaa8b84d63eeeb108c57cb8
Milton Pulido Mendieta <milton.pulido2018@gmail.com>
2026-05-21 15:34:45 -0500

    se emplean comentarios en la p├ígina de detalles de receta y cambios parciales en el formulario de ingredientes... falta poder agregar recetas con ingredientes que NO est├⌐n creados en la base de datos

--- src/services/rating.ts
+++ src/services/rating.ts
@@ -4,6 +4,8 @@ import api from "./axios";
 
 const RATINGS_URL = import.meta.env.VITE_API_URL+import.meta.env.VITE_RATING;
 const ADD_RATING = import.meta.env.VITE_BASE_URL+import.meta.env.VITE_ADD_RATING;
+const GET_RATINGS_DETAIL = import.meta.env.VITE_GET_RATING;
+
 const headers = {headers: {
     "Content-Type": "application/json"
   }};
@@ -26,3 +28,14 @@ export const addRating = async (rating : AddRating ) => {
         console.log(error);
     }
 } 
+
+export const getRatingsDetail = async (num: number) =>{
+    try {
+        const ratingDetail = await api.post(GET_RATINGS_DETAIL,num,headers)
+        const data = ratingDetail.data
+        return data
+    } catch (error) {
+        console.log(error);        
+    }
+}
+
